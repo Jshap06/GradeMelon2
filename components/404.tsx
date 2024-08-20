5adb0f3580200b7a784a7ef4b35e4abef9219601
Themightypotato <50190184+Themightypotato@users.noreply.github.com>
2024-08-19 22:16:04 -0400

    Add files via upload

---
+++ components/404.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+
+	return (
+		<div className="p-5 md:p-10">
+			<h1 className="dark:text-white font-bold text-4xl">
+				404: Found Not Found
+			</h1>
+		</div>
+	);
+}
